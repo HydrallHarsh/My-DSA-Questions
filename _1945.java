92e99372f6ffb6cde6d73bf463ef761177779059
HydrallHarsh <harshsantwani.cse21@aii.ac.in>
2024-09-03 14:16:04 +0530

    Updated _1945

--- _1945.java
+++ _1945.java
@@ -6,32 +6,34 @@ public class _1945 {
         String snew = "";
         int sum = 0;
         double b = 0;
-        double c = 0;
         for (int i = 0; i < s.length(); i++) {
             snew = snew + (s.charAt(i) - 'a' + 1);
         }
-
+        System.out.println(snew);
         b = Double.parseDouble(snew);
-        for (int i = 0; i < k; i++) {
+        System.out.println(b);
+        int i = 0;
+        String temp = "";
+        while(k-->0){
             sum = 0;
-            while (b > 0) {
-                c = b % 10;
-                sum = sum + (int) c;
-                b = (int) b / 10;
+            for (int j = 0; j < snew.length(); j++) {
+                temp += snew.charAt(j);
+                sum = sum + Integer.parseInt(temp);
+                temp = "";
             }
-            b = sum;
+            snew = Integer.toString(sum); 
         }
 
-        System.out.println(c);
+        // System.out.println(c);
         return sum;
     }
 
     // 2nd Logic
-    public static int getLucky2(String s, int k) {
-        List<char> snew  = new ArrayList<>();
-    }
+    // public static int getLucky2(String s, int k) {
+    //     List<char> snew  = new ArrayList<>();
+    // }
 
     public static void main(String[] args) {
-        System.out.println(getLucky2("dbvmfhnttvr", 5));
+        System.out.println(getLucky("iiii", 1));
     }
 }
