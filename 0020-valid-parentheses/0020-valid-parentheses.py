3a94576acf2482b05318d019ab7afcbac4fd5b8b
Harsh Santwani <96873014+HydrallHarsh@users.noreply.github.com>
2024-11-05 07:12:47 +0530

    Time: 0 ms (100%), Space: 16.7 MB (9.17%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = deque()
+        if(len(s) == 1):
+            return False
+        for i in s:
+            if(i == '(' or i == '{' or i=='['):
+                stack.append(i)
+            elif(i == ')' and len(stack) !=0 and stack[-1] == '('  ):
+                stack.pop()
+            elif(i == ']' and len(stack) !=0 and stack[-1] == '[' ):
+                stack.pop()
+            elif(i == '}' and len(stack) !=0 and stack[-1] == '{'  ):
+                stack.pop()
+            else:
+                return False
+        if(len(stack) == 0):
+            return True
+        else:
+            return False
