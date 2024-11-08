9de3918d3df7c804c8d52da6d1c05c0fc0e72e30
Harsh Santwani <96873014+HydrallHarsh@users.noreply.github.com>
2024-11-08 07:47:22 +0530

    Time: 47 ms (74.11%), Space: 32.7 MB (78.2%) - LeetHub

--- 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.py
+++ 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.py
@@ -2,11 +2,14 @@ class Solution:
     def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
         ans = []
         xor = 0
+        #Calculating XOR of the whole array nums
         for i in nums:
             xor = xor ^ i
+        #Calculates the value of k that maximises the result of XOR of array
         maskedBits = (1 << maximumBit)-1
         
         for i in reversed(nums):
             ans.append(xor ^ maskedBits)
+            #Removing Last element from the array
             xor = xor ^ i
         return ans
