9f834820b798a28232b7d50ea53c88c6660aafdd
Harsh Santwani <96873014+HydrallHarsh@users.noreply.github.com>
2025-10-28 16:28:55 +0530

    Time: 5556 ms (25%), Space: 12.5 MB (8.33%) - LeetHub

--- 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.py
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.py
@@ -10,24 +10,17 @@ class Solution(object):
             comp_nums.append(0)
             if nums[i] == 0:
                 l.append(i)
-        # print(l)
         for curr in l:
-            # print("============NEW========================")
-            # print(nums,nums1)
             nums1 = nums[:]
             curr_cp = curr
             t = 1
             while(0 <= curr_cp < n):
-                # print("initi:",curr_cp)
                 if(nums1[curr_cp] == 0):
                     curr_cp += t
-                    # print("o comp:",curr_cp,nums1)
                 elif(nums1[curr_cp]>0):
                     nums1[curr_cp] -= 1
                     t = t *(-1)
                     curr_cp += t
-                    # print(">o comp:",curr_cp,nums1)
-            # print("loop1", nums1)
             if(nums1 == comp_nums): res += 1
             nums1 = nums[:]
             curr_cp = curr
@@ -39,6 +32,5 @@ class Solution(object):
                     nums1[curr_cp] -= 1
                     t *= (-1)
                     curr_cp += t
-            # print("loop2", nums1)
             if(nums1 == comp_nums): res += 1
         return res
